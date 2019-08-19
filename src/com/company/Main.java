a8f6332de685e5440b2c22bc974f5fd64f188b3f
artsemy <artemiy.konoplyanik@gmail.com>
2019-08-19 19:00:51 +0300

    part 2 done

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,15 +3,33 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        part1();
+//        part1();
+//        part2();
+        part3();
     }
 
     private static void part1(){
         MyClass1 myClass1 = new MyClass1();
-//        myClass1.function1();
-//        myClass1.function2();
-//        myClass1.function3();
-//        myClass1.function4();
+        myClass1.function1();
+        myClass1.function2();
+        myClass1.function3();
+        myClass1.function4();
         myClass1.function5();
     }
+
+    private static void part2(){
+        MyClass2 myClass2 = new MyClass2();
+        myClass2.function1();
+        myClass2.function2();
+        myClass2.function3();
+        myClass2.function4();
+        myClass2.function5();
+        myClass2.function6();
+        myClass2.function7();
+        myClass2.function8();
+        myClass2.function9();
+        myClass2.function10();
+    }
+
+    static void part3(){}
 }
---
+++ src/com/company/MyClass2.java
@@ -0,0 +1,120 @@
+package com.company;
+
+class MyClass2 {
+
+    void function1(){
+        String str = "a df  dfef   dsfdf fsdf  sfd f";
+        String str2 = " ";
+        int k = 0;
+        for (int i = 1; i < str.length(); i++) {
+            if(str.contains(str2)){
+                k = i;
+            }
+            str2 = str2 + " ";
+        }
+        System.out.println(str);
+        System.out.println("the biggest sequence " + k);
+    }
+
+    void function2(){
+        String str = "asabtrareatfdaafa";
+        System.out.println("was " + str);
+        str = str.replaceAll("a", "ab");
+        System.out.println("now " + str);
+    }
+
+    void function3(){
+        String str = "absba";
+        String str2 = new StringBuffer(str).reverse().toString();
+        if (str.equals(str2)){
+            System.out.println(str + " is palindrome");
+        } else {
+            System.out.println(str + " is not palindrome");
+        }
+    }
+
+    void function4(){
+        String str = "информатика";
+        String str2 = "";
+        str2 = str2.concat(str.substring(7,8));
+        str2 = str2.concat(str.substring(3,5));
+        str2 = str2.concat(str.substring(7,8));
+        System.out.println(str2);
+    }
+
+    void function5(){
+        String str = "asabtrareatfdaafa";
+        System.out.print(str);
+        int count = 0;
+        while (str.contains("a")){
+            count++;
+            str = str.replaceFirst("a", "b");
+        }
+        System.out.println(" contains a " + count + " times");
+    }
+
+    void function6(){
+        String str = "arty";
+        String str2 = "";
+        for (int i = 0; i < str.length(); i++) {
+            str2 = str2.concat(str.substring(i,i+1));
+            str2 = str2.concat(str.substring(i,i+1));
+        }
+        System.out.println(str);
+        System.out.println(str2);
+    }
+
+    void function7(){
+        String str = "aa bc dgbas ewq  ioppoik";
+        System.out.println("was " + str);
+        String str2 = "";
+        str = str.replaceAll(" ", "");
+        while (str.length() > 0){
+            str2 = str2.concat(str.substring(0,1));
+            str = str.replaceAll(str.substring(0,1), "");
+        }
+        System.out.println("now " + str2);
+    }
+
+    void function8(){
+        String str = "1 22 333 4444 22 55555 333";
+        System.out.println(str);
+        String[] mass = str.split(" ");
+        int max = mass[0].length();
+        str = mass[0];
+        for (int i = 1; i < mass.length; i++) {
+            if (mass[i].length() > max){
+                str = mass[i];
+                max = mass[i].length();
+            }
+        }
+        System.out.println("the longest is " + str);
+    }
+
+    void function9(){
+        String str = "en_aaaABCDe_ru_ЯЯЯАААааа";
+        int countLower = 0;
+        int countUpper = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) > 96 && str.charAt(i) < 123){
+                countLower++;
+            }
+            if (str.charAt(i) > 64 && str.charAt(i) < 91){
+                countUpper++;
+            }
+        }
+        System.out.println("Upper " + countUpper + " Lower " + countLower);
+    }
+
+    void function10(){
+        String str = "aaa?bbb?cc?d.f.r.t!yy!u!" ;
+        int count = 0;
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if (ch == 33 || ch == 63 || ch == 46){
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
